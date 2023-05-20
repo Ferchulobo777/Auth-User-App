d98ac459d262fd5863cf2bad9f6a4e897ebdb1fb
Fernando Rodriguez <ferchulobo2015@gmail.com>
2023-05-20 01:38:49 -0300

    Auth User App

---
+++ src/routes/user.route.js
@@ -0,0 +1,33 @@
+const { getAll, create, getOne, remove, update, verifyCode, login, getLoggedUser, resetPassword, resetCode } = require('../controllers/user.controllers');
+const express = require('express');
+const verifyJWT = require('../utils/verifyJWT');
+
+const userRouter = express.Router();
+
+userRouter.route('/')
+    .get(verifyJWT,  getAll)
+    .post(create);
+
+userRouter.route('/verify/:code')
+    .get(verifyCode);
+    
+userRouter.route('/login')
+    .post(login);   
+
+userRouter.route('/me')
+    .get(verifyJWT, getLoggedUser);
+    
+userRouter.route('/reset_password')
+     .post(resetPassword);
+     
+userRouter.route('/reset_password/:code')
+     .post(resetCode);
+
+userRouter.route('/:id')
+    .get(verifyJWT, getOne)
+    .delete(verifyJWT, remove)
+    .put(verifyJWT, update);
+
+
+
+module.exports = userRouter;
